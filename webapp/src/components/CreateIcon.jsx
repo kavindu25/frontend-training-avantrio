60324ad7d19d213e200cd5a8fdb6429a685e50cf
kavindu25 <kavindu25@github.com>
2022-07-28 10:06:42 +0530

    fix: design page changes

--- webapp/src/components/CreateIcon.jsx
+++ webapp/src/components/CreateIcon.jsx
@@ -2,40 +2,16 @@ import React from "react";
 
 export default function CreateIcon({
   iconVariant,
+  innerIconVarient,
   bgColour,
-  bgIcon,
+  innerIconBg,
   customStyles,
-  bgIconCustomStyles,
+  innerIconCustomStyles,
 }) {
-  let iconStyle = "";
-  let iconStyleInner = "";
-
-  const getStyles = () => {
-    if (iconVariant === "type1") {
-      iconStyle = "icon-type-1";
-    } else if (iconVariant === "type2") {
-      iconStyle = "icon-type-2";
-    } else if (iconVariant === "type3") {
-      iconStyle = "icon-type-3";
-    }
-    return iconStyle;
-  };
-
-  const getInnerStyles = () => {
-    if (iconVariant === "type1") {
-      iconStyleInner = "icon-type-1-inner";
-    } else if (iconVariant === "type2") {
-      iconStyleInner = "icon-type-2-inner";
-    } else if (iconVariant === "type3") {
-      iconStyleInner = "icon-type-3-inner";
-    }
-    return iconStyleInner;
-  };
-
   return (
     <div>
       <div
-        className={getStyles()}
+        className={iconVariant}
         style={{
           ...customStyles,
           backgroundColor: bgColour,
@@ -44,12 +20,10 @@ export default function CreateIcon({
         }}
       >
         <div
-          className={getInnerStyles()}
+          className={innerIconVarient}
           style={{
-            ...bgIconCustomStyles,
-            backgroundImage: `url(${bgIcon})`,
-            // background:
-            //   "url(https://www.facebook.com/images/fb_icon_325x325.png)",
+            ...innerIconCustomStyles,
+            backgroundImage: `url(${innerIconBg})`,
           }}
         ></div>
       </div>
--- webapp/src/pages/DesignSystem.jsx
+++ webapp/src/pages/DesignSystem.jsx
@@ -3,6 +3,10 @@ import { Form, SubmitButton, TextField } from "../components/forms";
 import CreateIcon from "../components/CreateIcon";
 import { useModal } from "../hooks/modalsHooks";
 import { useGetMyProfileQuery } from "../services/userService";
+import {
+  ICON_TYPES_LIST,
+  INNER_ICON_TYPES_LIST,
+} from "../constants/iconTypeConsts";
 
 export default function DesignSystem() {
   const { showModal } = useModal();
@@ -10,20 +14,20 @@ export default function DesignSystem() {
   return (
     <div className="container">
       <div>
-        <div className="text-dark bg-light py-2 my-3">
-          <h1 className="display-6">Typography </h1>
+        <div className="text-dark bg-light py-4 my-3">
+          <h1 className="display-6 ms-5">Typography </h1>
         </div>
-        <div className="py-2">
+        <div className="py-2 ms-5">
           <h1 className="py-2">h1 size</h1>
           <h2 className="py-2">h2 size</h2>
           <h3 className="">h3 size</h3>
         </div>
       </div>
       <div>
-        <div className="text-dark bg-light py-2 my-3">
-          <h1 className="display-6">Fields</h1>
+        <div className="text-dark bg-light py-4 my-3">
+          <h1 className="display-6 ms-5">Fields</h1>
         </div>
-        <div className="py-2">
+        <div className="py-2 ms-5">
           <Form initialValues={{}}>
             <div className="row pt-2">
               <div className="col-3">
@@ -53,12 +57,12 @@ export default function DesignSystem() {
         </div>
       </div>
       <div>
-        <div className="text-dark bg-light py-2 my-3">
-          <h1 className="display-6">Buttons</h1>
+        <div className="text-dark bg-light py-4 my-3">
+          <h1 className="display-6 ms-5">Buttons</h1>
         </div>
-        <div className="py-2">
+        <div className="py-2 ms-5">
           <Form initialValues={{}}>
-            <div className="row pt-2">
+            <div className="row py-2">
               <div className="col-3">
                 <SubmitButton size="lg" />
               </div>
@@ -66,32 +70,17 @@ export default function DesignSystem() {
                 <SubmitButton variant="outlined" size="lg" />
               </div>
             </div>
-            {/* <div className="row pt-2">
-              <div className="col-3">
-                <button
-                  className="btn btn-primary"
-                  onClick={() => {
-                    showModal({
-                      modalType: "ExampleModalOne",
-                      modalProps: { title: "Test Modal One" },
-                    });
-                  }}
-                >
-                  Show Modal
-                </button>
-              </div>
-            </div> */}
           </Form>
         </div>
       </div>
       <div>
-        <div className="text-dark bg-light py-2 my-3">
-          <h1 className="display-6">Drop Shadows</h1>
+        <div className="text-dark bg-light py-4 my-3">
+          <h1 className="display-6 ms-5">Drop Shadows</h1>
         </div>
-        <div>
-          <div className="row">
+        <div className="py-2">
+          <div className="row ms-5">
             <div
-              className="card text-center mx-5 my-2"
+              className="card text-center my-2"
               style={{ width: "8rem", height: "5rem" }}
             >
               <div className="card-body">
@@ -109,9 +98,9 @@ export default function DesignSystem() {
               </div>
             </div>
           </div>
-          <div className="row">
+          <div className="row ms-5">
             <div
-              className="card text-center mx-5 my-4"
+              className="card text-center my-4"
               style={{ width: "18rem", height: "6rem" }}
             >
               <div className="card-body">
@@ -122,148 +111,200 @@ export default function DesignSystem() {
         </div>
       </div>
       <div>
-        <div className="text-dark bg-light py-2 my-3">
-          <h1 className="display-6">Colours</h1>
+        <div className="text-dark bg-light py-4 my-3">
+          <h1 className="display-6 ms-5">Colours</h1>
         </div>
-        <div className="my-2">
-          <h2 className="mx-5">Primary</h2>
-          <div className="row">
+        <div className="my-2 ">
+          <h2 className="ms-5">Primary</h2>
+          <div className="row ms-5">
             <div
-              className="card text-center bg-primary mx-5 my-2"
+              className="card text-center bg-primary my-2"
               style={{ width: "18rem", height: "5rem" }}
             ></div>
           </div>
         </div>
         <div>
-          <h2 className="mx-5">Secondary</h2>
-          <div className="row">
+          <h2 className="ms-5">Secondary</h2>
+          <div className="row ms-5">
             <div
-              className="card text-center bg-secondary mx-5 my-"
+              className="card text-center bg-secondary my-2"
               style={{ width: "18rem", height: "5rem" }}
             ></div>
           </div>
         </div>
       </div>
       <div>
-        <div className="text-dark bg-light py-2 my-3">
-          <h1 className="display-6">Icons</h1>
+        <div className="text-dark bg-light py-4 my-3">
+          <h1 className="display-6 ms-5">Icons</h1>
         </div>
         <div>
-          <div className="row pt-2">
-            <div className="col-2">
+          <div className="row py-4 ms-5">
+            <div className="col-1">
               <CreateIcon
-                iconVariant={"type1"}
+                iconVariant={ICON_TYPES_LIST.TYPE_1}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_1}
                 bgColour={"#FFB9D6"}
-                bgIcon={"/images/media.png"}
+                innerIconBg={"/images/media.png"}
               />
             </div>
-            <div className="col-2">
+            <div className="col-1">
               <CreateIcon
-                iconVariant={"type1"}
+                iconVariant={ICON_TYPES_LIST.TYPE_1}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_1}
                 bgColour={"#FFE3B9"}
-                bgIcon={"/images/music.png"}
+                innerIconBg={"/images/music.png"}
               />
             </div>
-            <div className="col-2">
+            <div className="col-1">
               <CreateIcon
-                iconVariant={"type1"}
+                iconVariant={ICON_TYPES_LIST.TYPE_1}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_1}
                 bgColour={"#C3FFB9"}
-                bgIcon={"/images/book.png"}
+                innerIconBg={"/images/book.png"}
               />
             </div>
-            <div className="col-2">
+            <div className="col-1">
               <CreateIcon
-                iconVariant={"type1"}
+                iconVariant={ICON_TYPES_LIST.TYPE_1}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_1}
                 bgColour={"#FFF8B9"}
-                bgIcon={"/images/doc.png"}
+                innerIconBg={"/images/doc.png"}
               />
             </div>
-            <div className="col-2">
+            <div className="col-1">
               <CreateIcon
-                iconVariant={"type1"}
+                iconVariant={ICON_TYPES_LIST.TYPE_1}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_1}
                 bgColour={"#B9EAFF"}
-                bgIcon={"/images/savings.png"}
+                innerIconBg={"/images/savings.png"}
               />
             </div>
-            <div className="col-2">
+            <div className="col-1">
               <CreateIcon
-                iconVariant={"type1"}
+                iconVariant={ICON_TYPES_LIST.TYPE_1}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_1}
                 bgColour={"#B9BCFF"}
-                bgIcon={"/images/work.png"}
+                innerIconBg={"/images/work.png"}
               />
             </div>
           </div>
-          <div className="row pt-4">
-            <div className="col-2">
-              <CreateIcon iconVariant={"type2"} bgIcon={"/images/bgfb.png"} />
-            </div>
-            <div className="col-2">
-              <CreateIcon iconVariant={"type2"} bgIcon={"google"} />
-            </div>
-            <div className="col-2">
+          <div className="row py-4 ms-5">
+            <div className="col-1">
               <CreateIcon
-                iconVariant={"type2"}
-                bgIcon={"/images/linkedin.png"}
+                iconVariant={ICON_TYPES_LIST.TYPE_2}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_2}
+                innerIconBg={"/images/facebook.png"}
+              />
+            </div>
+            <div className="col-1">
+              <CreateIcon
+                iconVariant={ICON_TYPES_LIST.TYPE_2}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_2}
+                innerIconBg={"/images/google.png"}
+              />
+            </div>
+            <div className="col-1">
+              <CreateIcon
+                iconVariant={ICON_TYPES_LIST.TYPE_2}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_2}
+                innerIconBg={"/images/linkedin.png"}
               />
             </div>
             <div className="col-2">
-              <CreateIcon iconVariant={"type2"} bgIcon={"/images/apple.png"} />
+              <CreateIcon
+                iconVariant={ICON_TYPES_LIST.TYPE_2}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_2}
+                innerIconBg={"/images/apple.png"}
+              />
             </div>
           </div>
-          <div className="row pt-4">
-            <div className="col-2">
-              <CreateIcon iconVariant={"type3"} bgIcon={"/images/bgfb.png"} />
-            </div>
-            <div className="col-2">
-              <CreateIcon iconVariant={"type3"} bgIcon={"google"} />
-            </div>
-            <div className="col-2">
+          <div className="row py-4 ms-5">
+            <div className="col-1">
               <CreateIcon
-                iconVariant={"type3"}
-                bgIcon={"/images/linkedin.png"}
+                iconVariant={ICON_TYPES_LIST.TYPE_3}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_3}
+                innerIconBg={"/images/share.png"}
               />
             </div>
-            <div className="col-2">
-              <CreateIcon iconVariant={"type3"} bgIcon={"/images/pencil.png"} />
+            <div className="col-1">
+              <CreateIcon
+                iconVariant={ICON_TYPES_LIST.TYPE_3}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_3}
+                innerIconBg={"/images/copy.png"}
+              />
             </div>
-            <div className="row pt-4">
-              <div className="col-2">
-                <CreateIcon
-                  customStyles={{
-                    width: "67px",
-                    height: "67px",
-                    borderRadius: "50%",
-                  }}
-                  bgColour={"#3FEA98"}
-                  bgIcon={"/images/tick.png"}
-                  bgIconCustomStyles={{
-                    width: "30px",
-                    height: "20px",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                  }}
-                />
-              </div>
-              <div className="col-2">
-                <CreateIcon
-                  customStyles={{
-                    width: "57px",
-                    height: "57px",
-                    borderRadius: "17.74px",
-                  }}
-                  // bgColour={"#30387D"}
-                  bgIcon={"/images/man-with-glasses.png"}
-                  bgIconCustomStyles={{
-                    width: "57.36px",
-                    height: "57.36px",
-                    borderRadius: "17.74px",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                  }}
-                />
-              </div>
+            <div className="col-1">
+              <CreateIcon
+                iconVariant={ICON_TYPES_LIST.TYPE_3}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_3}
+                innerIconBg={"/images/trash.png"}
+              />
+            </div>
+            <div className="col-1">
+              <CreateIcon
+                iconVariant={ICON_TYPES_LIST.TYPE_3}
+                innerIconVarient={INNER_ICON_TYPES_LIST.TYPE_3}
+                innerIconBg={"/images/pencil.png"}
+              />
+            </div>
+          </div>
+          {/* place-items: center */}
+          <div className="row py-4 ms-5">
+            <div className="col-1">
+              <CreateIcon
+                customStyles={{
+                  width: "67px",
+                  height: "67px",
+                  borderRadius: "50%",
+                  border: "2px solid #dadada",
+                }}
+                innerIconBg={"/images/bell.png"}
+                innerIconCustomStyles={{
+                  width: "18px",
+                  height: "22px",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                }}
+              />
+            </div>
+
+            <div className="col-1">
+              <CreateIcon
+                customStyles={{
+                  width: "67px",
+                  height: "67px",
+                  borderRadius: "50%",
+                }}
+                bgColour={"#3FEA98"}
+                innerIconBg={"/images/tick.png"}
+                innerIconCustomStyles={{
+                  width: "30px",
+                  height: "20px",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                }}
+              />
+            </div>
+            <div className="col-1">
+              <CreateIcon
+                customStyles={{
+                  width: "57px",
+                  height: "57px",
+                  borderRadius: "17.74px",
+                }}
+                // bgColour={"#30387D"}
+                innerIconBg={"/images/man-with-glasses.png"}
+                innerIconCustomStyles={{
+                  width: "57.36px",
+                  height: "57.36px",
+                  borderRadius: "17.74px",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                }}
+              />
             </div>
           </div>
         </div>
