ce0411900d018e33591f97ba325b732b3c9b0373
kavindu25 <kavindu25@github.com>
2022-08-17 14:23:16 +0530

    fix: color picker styling fixed

--- webapp/src/components/forms/ColorPicker.jsx
+++ webapp/src/components/forms/ColorPicker.jsx
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { useField } from "formik";
 
 export default function ColorPicker() {
+  const [colorName, setColorName] = useState("Select a colour");
+  const [selectBackground, setSelectBackground] = useState("#BC45A2");
+  const availableColors = [
+    {
+      name: "Purple",
+      value: "#BC45A2",
+    },
+    {
+      name: "Orange",
+      value: "#E16347",
+    },
+    {
+      name: "Green",
+      value: "#52A944",
+    },
+    {
+      name: "Yellow",
+      value: "#FFF8B9",
+    },
+    {
+      name: "Blue",
+      value: "#5698B5",
+    },
+    {
+      name: "Light blue",
+      value: "#B9BCFF",
+    },
+  ];
+
+  const onSelectChange = (e) => {
+    setColorName(e.target.value);
+    const selectedColor = availableColors.find(
+      ({ name }) => name === e.target.value
+    );
+    setSelectBackground(selectedColor.value);
+  };
   return (
     <div className="color-picker">
       <p className="para2 color-picker-label mb-2">Color Picker</p>
       <div className="color-picker-input-wrapper">
-        <input
-          type="color"
-          className="form-select color-selector"
-          list="colors"
-        />
-        <datalist id="colors">
-          <option>#BC45A2</option>
-          <option>#E16347</option>
-          <option>#52A944</option>
-          <option>#FFF8B9</option>
-          <option>#5698B5</option>
-          <option>#B9BCFF</option>
-        </datalist>
+        <p className="color-selected para1">{colorName}</p>
+        <select
+          className="color-select form-select"
+          onChange={onSelectChange}
+          style={{ backgroundColor: selectBackground }}
+        >
+          <option className="purple" value={"Purple"}></option>
+          <option className="orange" value={"Orange"}></option>
+          <option className="green" value={"Green"}></option>
+          <option className="yellow" value={"Yellow"}></option>
+          <option className="blue" value={"Blue"}></option>
+          <option className="light-blue" value={"Light blue"}></option>
+        </select>
       </div>
     </div>
   );
